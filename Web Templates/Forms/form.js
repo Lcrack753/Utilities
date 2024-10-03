9dd48494923c24a9f27cd1b48490c6c5a9726057
Lucio <luxiocarnero@gmail.com>
2024-10-03 11:35:47 -0300

    webtemplate form box and fileupload

--- Web Templates/Forms/form.js
+++ Web Templates/Forms/form.js
@@ -17,3 +17,15 @@ function showDatepicker(btn) {
   const input = btn.parentNode.querySelector('input');
   input.showPicker();
 }
+function showFileinput(btn) {
+  const input = btn.querySelector('input');
+  input.click()
+}
+function fileInputChange(fileInput) {
+  const fileInputContainer = fileInput.parentNode
+  if (fileInput.files.length > 0) {
+    fileInputContainer.classList.add("file-selected");
+  } else {
+    fileInputContainer.classList.remove("file-selected");
+  }
+}
